54fc77b866879df1b9ef4a2b3c662c348d07c196
Kamya Tanhaie <dgghost034@gmail.com>
2026-06-17 19:37:28 +0330

    working on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
+import style from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <aside>
-      <div>
-        <div>logo</div>
+    <aside className={style.aside}>
+      <div className="flex flex-col justify-between h-screen">
         <div>
-          <nav>
+          <div>logo</div>
+          <nav className="flex flex-col">
             <Link to="/dashboard">Dashboard</Link>
             <Link to="/transactions">Transactions</Link>
             <Link to="/budget">Budget</Link>
             <Link to="/categories">Categories</Link>
+            <Link to="/profile">Profile</Link>
           </nav>
         </div>
         <div>profile</div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,9 @@ import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>hello dashboard</h1>
+    <>
       <Sidebar />
-    </div>
+    </>
   );
 };
 
