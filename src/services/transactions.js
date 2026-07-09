83d148e630edbdeb456a71591758f766293253de
Kamya Tanhaie <dgghost034@gmail.com>
2026-07-09 23:38:21 +0330

    sorting for transactionsList added

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -9,7 +9,7 @@ export async function getAllTransactions() {
 
 export async function getTransactions(page = 1, perPage = 5) {
   const result = await api.get(
-    `/collections/transactions/records?page=${page}&perPage=${perPage}&expand=category`,
+    `/collections/transactions/records?page=${page}&perPage=${perPage}&expand=category&sort=-transactionDate`,
   );
   console.log(result, "transactions paginated result");
 
